7166b89b082557510da44865144ea12e44964c8a
Swyzsh <swyzsh@protonmail.com>
2024-04-27 21:00:35 +0530

    finish UI

--- app.js
+++ app.js
@@ -22,10 +22,20 @@ let turnP2 = false;
 
 function Gameboard(p1_mark, p2_mark) {
 
+  const cell0 = (document.querySelector(".index0")).textContent;
+  const cell1 = (document.querySelector(".index1")).textContent;
+  const cell2 = (document.querySelector(".index2")).textContent;
+  const cell3 = (document.querySelector(".index3")).textContent;
+  const cell4 = (document.querySelector(".index4")).textContent;
+  const cell5 = (document.querySelector(".index5")).textContent;
+  const cell6 = (document.querySelector(".index6")).textContent;
+  const cell7 = (document.querySelector(".index7")).textContent;
+  const cell8 = (document.querySelector(".index8")).textContent;
+
   let gameboard = [
-    ['[ ]', '[ ]', '[ ]'],
-    ['[ ]', '[ ]', '[ ]'],
-    ['[ ]', '[ ]', '[ ]']
+    [cell0, cell1, cell2],
+    [cell3, cell4, cell5],
+    [cell6, cell7, cell8]
   ]
 
   if (p1_mark === 'x' && p1_mark === 'X' && turnP1 == true) {
@@ -38,10 +48,30 @@ function Gameboard(p1_mark, p2_mark) {
 }
 
 function controller() {
-  const player1 = new Player('user', 'X');
-  const player2 = new Player('bot', 'Y');
-  
-  let game = new Gameboard(player1.mark, player2.mark);
-  
-  console.log(game);  
+  // handle player's mark choice
+  const markXbtn = document.getElementById("mark-x");
+  const markYbtn = document.getElementById("mark-o");
+
+  let initialMark = markXbtn;
+  let player1;
+  let player2;
+  let game;
+
+  if (initialMark == markXbtn) {
+    player1 = new Player('user', 'X');
+    player2 = new Player('bot', 'Y');
+    game = new Gameboard(player1.mark, player2.mark);
+
+    console.log(game);
+
+  } else if (initialMark == markYbtn) {
+    player1 = new Player('user', 'Y');
+    player2 = new Player('bot', 'X');
+    game = new Gameboard(player1.mark, player2.mark);
+
+    console.log(game);
+
+  }
 }
+
+controller();
